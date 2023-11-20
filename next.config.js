18c6a2dec2bd28ec98ae019a9ccaf3e3910a38e9
Anshdeep Singh <69358318+Anshdeep-Singh@users.noreply.github.com>
2023-11-20 01:47:59 -0800

    output specified

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    distDir: "out",
+    output: "export",
+    images: { unoptimized: true },
+}
 
 module.exports = nextConfig
