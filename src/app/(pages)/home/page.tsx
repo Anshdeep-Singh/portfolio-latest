0e8adec1ad88674114433e36c4e9b5c58c053c32
Anshdeep Singh <69358318+Anshdeep-Singh@users.noreply.github.com>
2023-11-20 02:05:55 -0800

    foundation complete

---
+++ src/app/(pages)/home/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to my Portfolio</h1>
+      <p>Thank you for visiting!</p>
+    </div>
+  );
+};
+
+export default HomePage;
