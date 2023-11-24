f957298ce56cfe42f31ee2871c00ef4ef5fe57cd
Anshdeep Singh <69358318+Anshdeep-Singh@users.noreply.github.com>
2023-11-23 23:06:04 -0800

    ui-changes

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -15,26 +15,34 @@ const HomePage = async () => {
     skills: ['JavaScript', 'TypeScript', 'React', 'Next.js', 'Tailwind CSS'],
   };
 
-  const profileImageRef = ref(storage, 'display_pics/Cat.png');
+  const profileImageRef = ref(storage, 'display_pics/anshdeep3.jpeg');
   
   const profileImageUrl = await getDownloadURL(profileImageRef);
 
   
   return (
     <div>      
-<header className="bg-gray-800 text-white p-4 flex">
-  <div className="max-w-3xl mx-auto">
-    <h1 className="text-3xl font-bold mb-2">{user.name}</h1> 
-    <p className="text-gray-400">{user.bio}</p>
-  </div>
+<header className="bg-black text-white pt-4 pb-4 flex">
+
+<div className="max-w-3xl mx-auto flex items-center">
+  <div className="inset-0">
   <img
-      src={profileImageUrl}
-      alt="Profile"
-      className="..." 
-    />
+    src={profileImageUrl}
+    alt="Profile"
+    className="md:w-360 md:h-80 rounded-full object-cover filter grayscale mix-blend-luminosity z-1"
+  />
+  </div>
+  <div className="flex-1 pl-4 z-10">
+    <h1 className="text-7xl font-bold mb-2 whitespace-nowrap">{user.name}</h1>
+    <p className="text-xl text-gray-400 text-center">{user.bio}</p>
+  </div>
+</div>
+
+
 </header>
       <main className="max-w-3xl mx-auto p-4">
         <div className="flex justify-between items-center mb-4">
+          
           <h2 className="text-xl font-medium">About</h2>
           <a href={user.website} className="text-blue-500 hover:underline">Website</a>
         </div>
--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,12 +1,11 @@
-
 import React from 'react';
 
 const Projects = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
-            <p className="text-lg text-gray-600 mb-8">Stay tuned for exciting updates!</p>
-            <div className="flex space-x-4">
+            <h1 className="text-4xl font-bold mb-4 text-center">Coming Soon</h1>
+            <p className="text-lg text-gray-600 mb-8 text-center">Stay tuned for exciting updates!</p>
+            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
                 <a href="#" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
                     Subscribe
                 </a>
