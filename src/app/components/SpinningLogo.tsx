caf1eb5d70b6f48786fb99502fa1c58ff39eeeb0
Anshdeep Singh <69358318+Anshdeep-Singh@users.noreply.github.com>
2023-12-12 02:18:56 -0800

    Update SpinningLogo.tsx
    updated the link from instagram to Global Chat App

--- src/app/components/SpinningLogo.tsx
+++ src/app/components/SpinningLogo.tsx
@@ -11,7 +11,7 @@ const SpinningLogo: React.FC = () => {
           <div className='w-48 h-auto display-flex items-center justify-center relative md:w-24 sm:w-24'>
         <SpinningText />
         <Link
-          href="https://www.instagram.com/anshdeep_singh_/"
+          href="https://chat.anshdeepsingh.com/"
           target='_blank'
           className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-black text-white shadow-md border border-solid border-black w-20 h-20 rounded-full hover:bg-white hover:text-black ml-1 md:ml-0 sm:hidden
           md:w-12 md:h-12 md:text-[10px]'
