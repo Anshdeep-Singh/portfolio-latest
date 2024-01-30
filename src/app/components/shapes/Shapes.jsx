c596da169780edff5d964126bc0ab186861e70dd
Anshdeep Singh <69358318+Anshdeep-Singh@users.noreply.github.com>
2024-01-29 20:39:33 -0800

    snake game integration

--- src/app/components/shapes/Shapes.jsx
+++ src/app/components/shapes/Shapes.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as THREE from 'three';
-import {Canvas} from 'react-three-fiber';
+import {Canvas} from '@react-three/fiber';
 import { ContactShadows, Float, Environment } from '@react-three/drei';
 import { Suspense, use, useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
